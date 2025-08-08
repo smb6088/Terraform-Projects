ab78a7d436bce0daebd85b5e47590fdbb1448e34
Siddharth Bhagvagar <111916691+smb6088@users.noreply.github.com>
2025-08-08 13:52:44 -0500

    fixed the vpc project-1

---
+++ Networking-infrastructure/vpc-public-subnet-IG/vpc.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "vpc-public-subnet-IG"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "Public1-Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "IG-wg"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
